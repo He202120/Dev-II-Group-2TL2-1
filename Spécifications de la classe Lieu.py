3be07a26f42412b4e0b5a2b2268f9142b44f9aba
CHARLIER Martin <m.charlier@students.ephec.be>
2023-12-14 17:43:30 +0100

    Update Spécifications de la classe Lieu.py
    Réecriture des spécifications de la classe lieu

--- Spécifications de la classe Lieu.py
+++ Spécifications de la classe Lieu.py
@@ -1,23 +1,28 @@
 class Lieu:
     def creer_map(self):
         """
-        Crée une carte du monde se focalisant sur la latitude et la longitude et la stocke dans un fichier html.
+        Crée une carte du monde se focalisant sur les attributs latitude et la longitude stocké dans l'objet courant pour stocker la carte dans un fichier html.
 
         PRE:
             - Aucun.
         POST:
-            - Permet de stocker dans un attribut de la classe Lieu une commande pour ouvrir le fichier html contenant la carte.
+            - Va creer un fichier portant l'id de l'objet courant et y stocker la carte.html.
+        RAISES:
+            -Renvoie une erreur NoLatLongError si l'objet courant ne possède pas de valeurs pour les attributs latitude et longitude.
         """
         # Implémentez le code pour créer la carte
         pass
 
     def lat_long(self, nom_ville, nom_rue, numero_maison):
         """
-        Renvoie la latitude et la longitude d'un endroit grâce au nom de la ville, le nom de la rue, et le numéro de maison.
+        Va modifier les attributs latitude et la longitude d'un endroit grâce aux attributs nom de la ville, le nom de la rue, et le numéro de maison qui sont stocké dans l'objet courant.
 
         PRE:
-            - nom_ville, nom_rue et numero_maison doivent être correctement orthographiés et exister.
+            - lors de l'initialisation, les attributs nom ville, nom rue et numero_maison doivent être correctement orthographiés et existés.
         POST:
-            - Renvoie une chaîne contenant la latitude et la longitude d'un lieu.
+            - Modifie l'objet courant pour y ajouter la latitude et la longitude d'un lieu dans leurs attributs respectifs.
+        RAISES:
+            -Renvoie une erreur ParamExceptionError si il y a une erreur dans les paramètres ou si aucun lieu n'a été trouvé.
+        
         """
         # Impl

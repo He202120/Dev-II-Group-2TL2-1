742d2c21098dfebaab3c1e65ff6e5ea5bc532390
Martin <charliermart1@gmail.com>
2023-12-28 17:28:41 +0100

    Nouvelle classe personne utilisé dans le programme

--- Spécification de la classe Personne.py
+++ Spécification de la classe Personne.py
@@ -1,80 +1,122 @@
-class Personne(Date):
-    def __init__(self, nom, prenom, date_naissance, id_nationalite, code_postal, rue, numero_maison):
-        """
-        Initialise une instance de la classe Personne.
+class personne(Dates):
+    def __init__(self, nom="", prenom="", addr=lieu(), age=0, tel="", nationalite="", idNationalite=""):
+        super().__init__()
+        self.__nom = nom
+        self.__prenom = prenom
+        self.__age = age
+        self.__addr = addr  # Adresse de la personne
+        self.__tel = tel
+        self.__nation = nationalite  # Nom de la nationalité
+        self.__identityCode = idNationalite  # numéro de registre de la carte d'identité
+        self.__id = 0
 
-        PRE:
-            - nom, prenom, date_naissance, id_nationalite, code_postal, rue, numero_maison doivent être des types valides.
-        POST:
-            - Une instance de la classe Personne est créée avec les attributs spécifiés.
-        """
-        self.dateobj = Date.__init__(self.date_naissance)
-        self.nom = nom
-        self.prenom = prenom
-        self.date_naissance = date_naissance
-        self.id_nationalite = id_nationalite
-        self.code_postal = code_postal
-        self.rue = rue
-        self.numero_maison = numero_maison
+    def __str__(self):
+        return f"Profil : {self.__id} / {self.__nom} {self.__prenom}\nAge : {self.__age}\nAdresse domicile : {self.__addr.getVille}\nTel. : {self.__tel}\nNationalité et code d'identité : {self.__nation} {self.__identityCode}\n"
 
-    def set_age(self, jour_annif, mois_annif, annee_annif):
+    def __eq__(self, autre):
+        return self.__addr.getVille == autre.getAdrr.getVille
+
+    @property
+    def getNom(self):
+        return self.__nom
+
+    @property
+    def getPrenom(self):
+        return self.__prenom
+
+
+    @property
+    def getmap(self):
+        try:
+            self.__addr.getmapPers(self.__id)
+        except ParamVideException:
+            print("aucune latitude et longitude renseigné")
+
+    @property
+    def getAdrr(self):
+        return self.__addr
+
+    @property
+    def get_age(self):
+        """
+               Calcule l'âge de la personne en utilisant la date actuelle et sa date de naissance.
+
+               PRE:
+                   - Aucun
+               POST:
+                   - Renvoie un entier représentant l'âge de la personne calculé en années en utilisant les attributs Jour, Mois, Année et dateactuel stocké dans l'objet dateobj
+               RAISES:
+                   - Renvoie une erreur ValueError si la date de naissance de la personne est dans le futur.
+               """
+        today = datetime.today()
+        annee_courante = today.strftime('%Y')
+        return int(annee_courante) - self.get_annee
+
+    @property
+    def getTel(self):
+        return self.__tel
+
+    @property
+    def getNat(self):
+        return self.__nation
+
+    @property
+    def getIdC(self):
+        return self.__id
+
+
+    @getNom.setter
+    def getNom(self, valeur):
+        if isinstance(valeur, str):
+            self.__nom = valeur
+
+
+    @getAdrr.setter
+    def getAdrr(self,valeur):
+        if isinstance(valeur, object):
+            self.__addr = valeur
+        else:
+            raise ValueError("addresse renseignée n'est pas un objet")
+
+    @getPrenom.setter
+    def getPrenom(self, valeur):
+        if isinstance(valeur, str):
+            self.__prenom = valeur
+
+    def set_age(self, valeur):
         """
         Modifie la date de naissance d'une personne qui se trouve dans les attributs Jour, Mois, Année interne à l'objet dateobj de classe Date.
 
         PRE:
-            - 
+            -
         POST:
             - Modifie l'attribut dateobj qui est un objet de classe Date stocké dans l'objet courant par les paramètre de cette fonction jour_annif, mois_annif, annee_annif.
         RAISES:
             - Renvoie une erreur ParamExceptionError si les attributs ne respectent pas les conditions ou que la date se trouve dans le futur.
         """
-        # Implémentez le code pour modifier la date de naissance
-        pass
+        today = datetime.today()
+        annee_courante = today.strftime('%Y')
+        try:
+            dates = datetime.strptime(valeur, "%d-%m-%Y")
+        except ValueError:
+            raise MauvaisDateException
+        except TypeError:
+            raise MauvaisDateException
+        if dates.year > int(annee_courante):
+            raise MauvaisDateException
+        self.get_annee = dates.year
+        self.get_mois = dates.month
+        self.get_jour = dates.day
 
-    def get_age(self):
-        """
-        Calcule l'âge de la personne en utilisant la date actuelle et sa date de naissance.
+    @getTel.setter
+    def getTel(self, valeur):
+        self.__Tel = valeur
 
-        PRE:
-            - Aucun
-        POST:
-            - Renvoie un entier représentant l'âge de la personne calculé en années en utilisant les attributs Jour, Mois, Année et dateactuel stocké dans l'objet dateobj
-        RAISES:
-            - Renvoie une erreur ValueError si la date de naissance de la personne est dans le futur.
-        """
-        # Implémentez le code pour calculer l'âge
-        pass
+    @getNat.setter
+    def getNat(self, valeur):
+        if isinstance(valeur, str):
+            self.__nation = valeur
 
-    def get_nationalite(self):
-        """
-        Obtient la nationalité d'une personne grâce à l'id nationalité de la personne.
-
-        PRE:
-            - Aucun.
-        POST:
-            - Renvoie la nationalité de la personne.
-        """
-        # Implémentez le code pour obtenir la nationalité
-        pass
-
-    def nom_complet(self):
-        """
-        Renvoie le nom et le prénom d'une personne en une phrase.
-
-        PRE:
-            - Aucun.
-        POST:
-            - Renvoie une chaîne représentant le nom et le prénom d'une personne.
-        """
-        return f"{self.nom} {self.prenom}"
-
-    def get_adresse(self):
-        """
-        Obtient l'adresse complète d'une personne grâce à son code postal, sa rue, son numéro de maison.
-
-        PRE:
-            - Aucun.
-        POST:
-            - Renvoie une chaîne représentant l'adresse d'une personne.
-        """
-        return f"{self.numero_maison}, {self.rue}, {self.code_postal}"
+    @getIdC.setter
+    def getIdC(self, valeur):
+        self.__id = valeur

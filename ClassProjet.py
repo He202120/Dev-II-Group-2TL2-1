d502f8da406f6d4e95fbcae8965d34d65064fdb9
Martin <charliermart@outlook.fr>
2024-01-06 17:53:12 +0100

    Implémentation des classes terminé

--- ClassProjet.py
+++ ClassProjet.py
@@ -1,9 +1,9 @@
 # Librairies
 
 from datetime import datetime
-import os
 from geopy.geocoders import Nominatim
 import folium
+import os
 
 
 class ParamVideException(Exception):
@@ -40,8 +40,8 @@ class lieu():
         self.__codePostal = codePostal
         self.__rueNum = rueNum
         self.__nbrmaison = nbrmaison
-        self.__latitude = self.lat()
-        self.__longitude = self.long()
+        self.__latitude = 0
+        self.__longitude = 0
 
     def __str__(self):
         return f"Ville : {self.__ville} \nCode postal : {self.__codePostal} \nRue et numéro : {self.__rueNum} {self.__nbrmaison}"
@@ -63,9 +63,12 @@ class lieu():
         return self.__nbrmaison
 
     @property
-    def set_Lat_Long(self):
-        self.__latitude = self.lat()
-        self.__longitude = self.long()
+    def get_lat(self):
+        return self.__latitude
+
+    @property
+    def get_long(self):
+        return self.__longitude
 
     @getVille.setter
     def getVille(self, valeur):
@@ -87,56 +90,52 @@ class lieu():
     def getNum(self, valeur):
         self.__nbrmaison = valeur
 
-    def lat(self):
+    def lat_long(self):
+        """
+        Va modifier les attributs latitude et la longitude d'un endroit grâce aux attributs nom de la ville, le nom de la rue, et le numéro de maison qui sont stocké dans l'objet courant.
+
+        PRE:
+            - lors de l'initialisation, les attributs nom ville, nom rue et numero_maison doivent être correctement orthographiés et existés.
+        POST:
+            - Modifie l'objet courant pour y ajouter la latitude et la longitude d'un lieu dans leurs attributs respectifs.
+        RAISES:
+            -Renvoie une erreur ParamExceptionError si il y a une erreur dans les paramètres ou si aucun lieu n'a été trouvé.
+
+        """
         # Initialiser le géocodeur Nominatim
-        geolocator = Nominatim(user_agent="mon_script",scheme="https")
+        geolocator = Nominatim(user_agent="mon_script", scheme="https")
 
         nom_ville = self.__ville
         nom_rue = self.__rueNum
         numero_maison = self.__nbrmaison
 
-        try:
-            # Concaténer le nom de la ville, de la rue et le numéro de maison pour la recherche
-            adresse_complete = f"{numero_maison} {nom_rue}, {nom_ville}"
+        # Concaténer le nom de la ville, de la rue et le numéro de maison pour la recherche
+        adresse_complete = f"{numero_maison} {nom_rue}, {nom_ville}"
 
-            # Obtenir les coordonnées à partir de l'adresse complète
-            location = geolocator.geocode(adresse_complete)
+        # Obtenir les coordonnées à partir de l'adresse complète
+        location = geolocator.geocode(adresse_complete)
 
-            if location:
-                # Afficher les coordonnées
-                return location.latitude
-            else:
-                return ""
+        if location:
+            # Afficher les coordonnées
+            self.__latitude = location.latitude
+            self.__longitude = location.longitude
+        else:
+            raise MauvaiseValeurException
 
-        except Exception as e:
-            print(f"Une erreur s'est produite : {e}")
 
-    def long(self):
-        # Initialiser le géocodeur Nominatim
-        geolocator = Nominatim(user_agent="mon_script",scheme="https")
-
-        nom_ville = self.__ville
-        nom_rue = self.__rueNum
-        numero_maison = self.__nbrmaison
-
-        try:
-            # Concaténer le nom de la ville, de la rue et le numéro de maison pour la recherche
-            adresse_complete = f"{numero_maison} {nom_rue}, {nom_ville}"
-
-            # Obtenir les coordonnées à partir de l'adresse complète
-            location = geolocator.geocode(adresse_complete)
-
-            if location:
-                # Afficher les coordonnées
-                return location.longitude
-            else:
-                return ""
-
-        except Exception as e:
-            print(f"Une erreur s'est produite : {e}")
 
     def getmapEnq(self, id):
-        if self.__latitude != "":
+        """
+        Crée une carte du monde se focalisant sur les attributs latitude et la longitude stocké dans l'objet courant pour stocker la carte dans un fichier html.
+
+        PRE:
+            - Aucun.
+        POST:
+            - Va creer un fichier portant l'id de l'objet courant et y stocker la carte.html.
+        RAISES:
+            -Renvoie une erreur NoLatLongError si l'objet courant ne possède pas de valeurs pour les attributs latitude et longitude.
+        """
+        if self.__latitude != 0:
             # Coordonné GPS (latitude, longitude) de l'emplacement souhaité
             latitude = self.__latitude
             longitude = self.__longitude
@@ -151,10 +150,20 @@ class lieu():
             ma_carte.save(f"lieux-enquetes/{id}.html")
             print("La map a bien été crée")
         else:
-            print("aucune latitude ou longitude n'ont été renseigné")
+            raise MauvaiseValeurException
 
     def getmapPers(self, id):
-        if self.__latitude != "":
+        """
+        Crée une carte du monde se focalisant sur les attributs latitude et la longitude stocké dans l'objet courant pour stocker la carte dans un fichier html.
+
+        PRE:
+            - Aucun.
+        POST:
+            - Va creer un fichier portant l'id de l'objet courant et y stocker la carte.html.
+        RAISES:
+            -Renvoie une erreur NoLatLongError si l'objet courant ne possède pas de valeurs pour les attributs latitude et longitude.
+        """
+        if self.__latitude != 0:
             # Coordonné GPS (latitude, longitude) de l'emplacement souhaité
             latitude = self.__latitude
             longitude = self.__longitude
@@ -169,7 +178,7 @@ class lieu():
             ma_carte.save(f"lieux-personnes/{id}.html")
             print("La map a bien été crée")
         else:
-            print("aucune latitude ou longitude n'ont été renseigné")
+            raise MauvaiseValeurException
 
 
 class Dates:
@@ -177,9 +186,6 @@ class Dates:
         self.__annee = 0
         self.__mois = 0
         self.__jour = 0
-
-    def __str__(self):
-        return f"{self.__jour}-{self.__mois}-{self.__annee}"
 
     @property
     def get_annee(self):
@@ -525,7 +531,4 @@ listeEnq = {}
 listePrs = {}
 listeSus = {}
 
-ma = personne()
-ma.set_age("26-02-2021")
-print(ma.get_age)
-print(ma.get_annee)
+
